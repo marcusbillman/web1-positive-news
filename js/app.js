6c52bf1adfbd32d94c506178e16b4f53324c0d3d
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2019-11-28 17:41:30 +0100

    revert "refactor js"
    This reverts commit dce43419ead0187a866e45a9e40aafa1a49ec389.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
-(function() {
-    var hamburgerIconLine1 = document.getElementById("hamburger-icon__line-1");
-    var hamburgerIconLine2 = document.getElementById("hamburger-icon__line-2");
-    var hamburgerIconLine3 = document.getElementById("hamburger-icon__line-3");
-    var menu = document.getElementById("menu");
-    var dimOverlay = document.getElementById("dim-overlay");
+var hamburgerIconLine1 = document.getElementById("hamburger-icon__line-1");
+var hamburgerIconLine2 = document.getElementById("hamburger-icon__line-2");
+var hamburgerIconLine3 = document.getElementById("hamburger-icon__line-3");
+var menu = document.getElementById("menu");
+var dimOverlay = document.getElementById("dim-overlay");
 
-    function toggleMenu() {
-        hamburgerIconLine1.classList.toggle("menu-open");
-        hamburgerIconLine2.classList.toggle("menu-open");
-        hamburgerIconLine3.classList.toggle("menu-open");
-        menu.classList.toggle("menu-open");
-        dimOverlay.classList.toggle("menu-open");
-    }
+function toggleMenu() {
+    hamburgerIconLine1.classList.toggle("menu-open");
+    hamburgerIconLine2.classList.toggle("menu-open");
+    hamburgerIconLine3.classList.toggle("menu-open");
+    menu.classList.toggle("menu-open");
+    dimOverlay.classList.toggle("menu-open");
+}
 
-    function closeMenu() {
-        hamburgerIconLine1.classList.remove("menu-open");
-        hamburgerIconLine2.classList.remove("menu-open");
-        hamburgerIconLine3.classList.remove("menu-open");
-        menu.classList.remove("menu-open");
-        dimOverlay.classList.remove("menu-open");
-    }
-})();
+function closeMenu() {
+    hamburgerIconLine1.classList.remove("menu-open");
+    hamburgerIconLine2.classList.remove("menu-open");
+    hamburgerIconLine3.classList.remove("menu-open");
+    menu.classList.remove("menu-open");
+    dimOverlay.classList.remove("menu-open");
+}
