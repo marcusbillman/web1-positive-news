84abf47069171d9b49ab053f152b043f3ccdd70e
Marcus Billman <xfoxx37@gmail.com>
2019-12-12 13:28:15 +0100

    calibrate article scroll indicator, fix js errors

--- js/app.js
+++ js/app.js
@@ -5,51 +5,54 @@ var menu = document.getElementById("menu");
 var dimOverlay = document.getElementById("dim-overlay");
 var logo = document.getElementById("logo");
 var logoOverlay = document.getElementById("logo-overlay");
+var article = document.getElementById("article");
 
 document.addEventListener("DOMContentLoaded", function() {
     var dateSpan = document.getElementById("date");
-    months = new Array(
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    );
-    var d = new Date();
-    var date = d.getDate();
-    var month = months[d.getMonth()];
-    dateSpan.textContent = date + " " + month;
+    if (dateSpan) {
+        months = new Array(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        );
+        var d = new Date();
+        var date = d.getDate();
+        var month = months[d.getMonth()];
+        dateSpan.textContent = date + " " + month;
 
-    var greetingHeading = document.getElementById("greeting");
-    var midnight = new Date(
-        d.getFullYear(),
-        d.getMonth(),
-        d.getDate(),
-        0,
-        0,
-        0
-    );
-    var hoursSinceMidnight = (d.getTime() - midnight.getTime()) / 3600000;
-    var greeting;
-    if (hoursSinceMidnight > 4 && hoursSinceMidnight < 10) {
-        greeting = "Good morning! 🌞";
-    } else if (hoursSinceMidnight < 13) {
-        greeting = "Hi there! 👋";
-    } else if (hoursSinceMidnight < 18) {
-        greeting = "Good afternoon! ✨";
-    } else if (hoursSinceMidnight < 23) {
-        greeting = "Good evening! ☕";
-    } else {
-        greeting = "Good night! 🌛";
+        var greetingHeading = document.getElementById("greeting");
+        var midnight = new Date(
+            d.getFullYear(),
+            d.getMonth(),
+            d.getDate(),
+            0,
+            0,
+            0
+        );
+        var hoursSinceMidnight = (d.getTime() - midnight.getTime()) / 3600000;
+        var greeting;
+        if (hoursSinceMidnight > 4 && hoursSinceMidnight < 10) {
+            greeting = "Good morning! 🌞";
+        } else if (hoursSinceMidnight < 13) {
+            greeting = "Hi there! 👋";
+        } else if (hoursSinceMidnight < 18) {
+            greeting = "Good afternoon! ✨";
+        } else if (hoursSinceMidnight < 23) {
+            greeting = "Good evening! ☕";
+        } else {
+            greeting = "Good night! 🌛";
+        }
+        greetingHeading.textContent = greeting;
     }
-    greetingHeading.textContent = greeting;
 });
 
 function toggleMenu() {
@@ -71,10 +74,10 @@ function closeMenu() {
 window.onscroll = function() {
     var scrolledDistance =
         document.body.scrollTop || document.documentElement.scrollTop;
-    var pageHeight =
-        document.documentElement.scrollHeight -
-        document.documentElement.clientHeight;
-    var scrolledPercentage = (scrolledDistance / pageHeight) * 100;
+    if (article) {
+        var articleHeight = article.scrollHeight;
+    }
+    var scrolledPercentage = (scrolledDistance / articleHeight) * 100;
 
     closeMenu();
     if (scrolledDistance > 30) {
@@ -86,6 +89,8 @@ window.onscroll = function() {
         menu.classList.remove("menu--shadow");
         logo.style.opacity = "1";
     }
-    logoOverlay.style.clipPath =
-        "inset(0 " + (100 - scrolledPercentage) + "% 0 0)";
+    if (logoOverlay) {
+        logoOverlay.style.clipPath =
+            "inset(0 " + (100 - scrolledPercentage) + "% 0 0)";
+    }
 };
